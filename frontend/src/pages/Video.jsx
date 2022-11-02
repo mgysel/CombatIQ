64e6b1aa4ed0701f5ed5ce2c8419060a48eb1732
minzhang0912 <minzhang0912@gmail.com>
2022-11-02 13:29:38 +0100

    Page updated
    Mainly Fighters and Video files have been updated

--- frontend/src/pages/Video.jsx
+++ frontend/src/pages/Video.jsx
@@ -3,13 +3,76 @@ import { useLocation } from "react-router-dom";
 import {
   Heading,
 } from "@chakra-ui/react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  MenuIcon,
+  MenuCommand
+} from '@chakra-ui/react'
+import { Button, Image } from "@chakra-ui/react";
 
 const Video = () => {
   
   return (
-    <Heading align="center" my="1rem">
-      Video
-    </Heading>
+    <><Heading align="center" my="1rem">
+      Please upload a video
+    </Heading><Tabs variant='enclosed'>
+        <TabList>
+          <Tab>Bio</Tab>
+          <Tab>Round 1</Tab>
+          <Tab>Round N</Tab>
+          <Tab>Overall</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>one!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>three!</p>
+          </TabPanel>
+          <TabPanel>
+            <p>Some introductions of this page</p>
+            <Menu>
+              <MenuButton as={Button} rightIcon='▾'>
+                Select a metric
+              </MenuButton>
+              <MenuList>
+                <MenuItem minH='48px'>
+                  <Image
+                    boxSize='2rem'
+                    borderRadius='full'
+                    src='https://placekitten.com/100/100'
+                    alt='Strikes'
+                    mr='12px'
+                  />
+                  <span>Strikes</span>
+                </MenuItem>
+                <MenuItem minH='40px'>
+                  <Image
+                    boxSize='2rem'
+                    borderRadius='full'
+                    src='https://placekitten.com/120/120'
+                    alt='Significant Strikes'
+                    mr='12px'
+                  />
+                  <span>Significant Strikes</span>
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </TabPanel>
+        </TabPanels>
+      </Tabs></>
   );
 };
 
