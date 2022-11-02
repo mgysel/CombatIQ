43c7df7816724c5ec4087d5c3184023d390421f6
Mike Gysel <z5251938@ad.unsw.edu.au>
2022-11-02 13:41:28 +0100

    Added Multiline graph and Video input

--- frontend/src/pages/Video.jsx
+++ frontend/src/pages/Video.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import {
+  Flex,
   Heading,
+  VStack,
 } from "@chakra-ui/react";
+import VideoInput from '../components/VideoInput.jsx'
 
 const Video = () => {
   
   return (
-    <Heading align="center" my="1rem">
-      Video
-    </Heading>
+    <VStack>
+      <Heading align="center" my="1rem">
+        Video
+      </Heading>
+      <Flex>
+        <VideoInput width={400} height={300} />
+      </Flex>
+    </VStack>
   );
 };
 
