32cf1e0142f72bd7c3b283c2f5ba7c154ce2ea5a
minzhang0912 <minzhang0912@gmail.com>
2022-11-16 15:52:29 +0100

    Add a sidebar component & update trend page

--- frontend/src/pages/Trends.jsx
+++ frontend/src/pages/Trends.jsx
@@ -1,63 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, ReactNode} from "react";
 import { useLocation } from "react-router-dom";
 import {
   Center,
   Heading,
-  HStack,
   VStack,
 } from "@chakra-ui/react";
 import MultiLine from "../components/visualisations/MultiLine";
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  MenuIcon,
-  MenuCommand
-} from '@chakra-ui/react'
-import { Button, Image } from "@chakra-ui/react";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Trends = () => {
   
   return (
-    
-    <VStack>
-      <Heading align="center" my="1rem">
-        Improvement Over Time
-      </Heading>
-      <Menu>
-        <MenuButton as={Button} rightIcon='▾'>
-          Select a metric
-        </MenuButton>
-        <MenuList>
-          <MenuItem minH='48px'>
-            <Image
-              boxSize='2rem'
-              borderRadius='full'
-              src='https://placekitten.com/100/100'
-              alt='Strikes'
-              mr='12px'
-            />
-            <span>Strikes</span>
-          </MenuItem>
-          <MenuItem minH='40px'>
-            <Image
-              boxSize='2rem'
-              borderRadius='full'
-              src='https://placekitten.com/120/120'
-              alt='Significant Strikes'
-              mr='12px'
-            />
-            <span>Significant Strikes</span>
-          </MenuItem>
-        </MenuList>
-      </Menu>
-      <MultiLine />
-    </VStack>
+
+      <VStack>
+        <Heading align="center" my="1rem">
+          Improvement Over Time
+        </Heading>
+        <MultiLine />
+        <Sidebar />
+      </VStack>
+
 
   );
 };
