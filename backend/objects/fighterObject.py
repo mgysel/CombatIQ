18461be2a55385fc1426a579fa55d12b469fff92
Mike Gysel <z5251938@ad.unsw.edu.au>
2022-11-25 11:09:12 +0100

    Can register fighter to backend

--- backend/objects/fighterObject.py
+++ backend/objects/fighterObject.py
@@ -19,11 +19,93 @@ class Fighter:
     '''
     User class that contains basic user info/methods
     '''
-    def __init__(self, _id, name, age, weight):
+    def __init__(self, _id, email, password, first_name, last_name, age, weight):
         self._id = _id
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.email = email
+        self.password = password
         self.age = age
         self.weight = weight
+
+    def get_all_fighters():
+        '''
+        Returns list of User objects from the database
+        '''
+        db = MongoWrapper().client['CombatIQ']
+        coll = db['Fighters']
+        fighters = []
+        for fighter_json in coll.find():
+            print("FIGHTER JSON")
+            print(fighter_json)
+            fighter = Fighter.from_json(fighter_json)
+            fighters.append(fighter)
+        return fighters
+
+    @staticmethod
+    def insert_one(user):
+        '''
+        Inserts a user object into the database
+        '''
+        json_obj = user.to_json()
+        if json_obj != None:
+            db = MongoWrapper().client['CombatIQ']
+            coll = db['Fighters']
+            try:
+                inserted = coll.insert_one(json_obj)
+                return inserted.inserted_id
+            except:
+                return None
+
+    @classmethod
+    def find_fighter_by_attribute(cls, attribute, user_attribute):
+        '''
+        Finds a user by a specific attribute
+        Returns user object
+        '''
+        db = MongoWrapper().client['CombatIQ']
+        coll = db['Fighters']
+        fighter_json = coll.find_one({ attribute: user_attribute })
+
+        if fighter_json:
+            user = Fighter.from_json(fighter_json)
+            return user
+        return None
+
+    @classmethod
+    def find_fighters_from_search(cls, query, page):
+        """
+        Returns users with matching name
+        """
+        PAGE_SIZE = 10
+        filter = {
+            '$or':
+                [
+                    {'first_name': {'$regex': f".*{query}.*", '$options': 'i'}},
+                    {'last_name': {'$regex': f".*{query}.*", '$options': 'i'}},
+                    {'email': {'$regex': f".*{query}.*", '$options': 'i'}}
+                ]
+        }
+        skip = (int(page)-1)*PAGE_SIZE
+        limit = PAGE_SIZE
+
+        db = MongoWrapper().client['CombatIQ']
+        coll = db['Fighters']
+        results = coll.find(filter=filter,skip=skip,limit=limit).collation({'locale':'en'}).sort([('first_name',1),('last_name',1)])
+
+        return [Fighter.from_json(x) for x in results]
+
+    # def to_json(self):
+    #     '''
+    #     User object to json object
+    #     NOTE: converts ObjectId to string
+    #     '''
+    #     obj = self.__dict__
+    #     if obj['_id'] == None:
+    #         del obj['_id']
+    #     else:
+    #         obj['_id'] = str(obj['_id'])
+    #     return obj
 
     @staticmethod
     def from_json(fighter_json):
@@ -39,18 +121,6 @@ class Fighter:
             if '_id' in fighter_json:
                 _id = fighter_json['_id']
             return Fighter(_id, fighter_json['name'], fighter_json['age'], fighter_json['weight'])
-
-    # def to_json(self):
-    #     '''
-    #     User object to json object
-    #     NOTE: converts ObjectId to string
-    #     '''
-    #     obj = self.__dict__
-    #     if obj['_id'] == None:
-    #         del obj['_id']
-    #     else:
-    #         obj['_id'] = str(obj['_id'])
-    #     return obj
 
     @staticmethod
     def many_to_json(fighters):
@@ -92,31 +162,81 @@ class Fighter:
             obj['_id'] = str(obj['_id'])
         return obj
 
-    def get_all_fighters():
+    ########## Checking user validity ##########
+    @classmethod
+    def unused_email(cls, user_email):
         '''
-        Returns list of User objects from the database
+        Determines if the supplied email is unused
+        Returns True if email unused
+        Returns False if email taken
         '''
         db = MongoWrapper().client['CombatIQ']
-        coll = db['Fighters']
-        fighters = []
-        for fighter_json in coll.find():
-            print("FIGHTER JSON")
-            print(fighter_json)
-            fighter = Fighter.from_json(fighter_json)
-            fighters.append(fighter)
-        return fighters
+        coll = db['users']
+        return(coll.find_one({ 'email': user_email }) is None)
 
-    # @staticmethod
-    # def insert_one(user):
-    #     '''
-    #     Inserts a user object into the database
-    #     '''
-    #     json_obj = user.to_json()
-    #     if json_obj != None:
-    #         db = MongoWrapper().client['vinyl_store']
-    #         coll = db['users']
-    #         try:
-    #             inserted = coll.insert_one(json_obj)
-    #             return inserted.inserted_id
-    #         except:
-    #             return None
+    @classmethod
+    def valid_email(cls, email):
+        '''
+        Checks that email is valid - per regex below
+        Checks that email is not None. 
+        Original code from:
+        https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
+        '''
+        regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+
+        # Compares email to regex format
+        if email is not None and re.search(regex, email):
+            return True
+
+        return False
+
+    @classmethod
+    def valid_password(cls, password):
+        '''
+        Checks that password is valid - > 6 characters
+        Checks that password is not None
+        '''
+        if password is not None and len(password) > 6:
+            return True
+
+        return False
+
+    @classmethod
+    def valid_matching_passwords(cls, password, confirm_password):
+        '''
+        Checks that passwords match
+        Checks that confirm password is not None
+        '''
+        return confirm_password is not None and password == confirm_password
+
+    @classmethod
+    def valid_name(cls, name):
+        '''
+        Checks that name is valid - between 1 and 50 characteds
+        Checks that name is not None. 
+        Original code from:
+        https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
+        '''
+        if name is not None and len(name) >= 1 and len(name) <= 50:
+            return True
+
+        return False
+
+    @classmethod
+    def is_valid_user(cls, user):
+        '''
+        Determines if user object is valid
+        Returns True if valid, False otherwise
+        '''
+        attributes = ['_id', 'email', 'password', 'first_name', 'last_name', 'payment_methods', 'shipping_address', 'spotify_id', 'cart', 'role', 'reset_code']
+        for attribute in attributes:
+            if not hasattr(user, attribute):
+                return False
+        return True
+
+    @classmethod
+    def is_valid_id(cls, _id):
+        '''
+        Determines if _id is 24-character hex string
+        '''
+        return len(_id) == 24
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, url_for, make_response
 from flask_mail import Mail, Message
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 # imports for PyJWT authentication
 import jwt
 from json import dumps, load
@@ -8,18 +8,110 @@ from functools import wraps
 import sys
 from werkzeug.security import generate_password_hash
 from pymongo import MongoClient
+from flask.json import jsonify
+from bson.objectid import ObjectId
+
 
 from objects.fighterObject import Fighter
+
 
 APP = Flask(__name__)
 # Allows cross-origin AJAX, so React can talk to this API
 CORS(APP)
-
-
 APP.config['SECRET_KEY'] = 'your secret key'
+APP.config['CORS_HEADERS'] = 'Content-Type'
+
+from auth import auth_register, auth_login, auth_request, auth_reset
+
+
+
+########## AUTH ROUTES ##########
+
+# decorator for verifying the JWT 
+def token_required(f): 
+    '''
+    Decorator for verifying the JWT
+    Use of JWT references:
+    https://www.geeksforgeeks.org/using-jwt-for-user-authentication-in-flask/
+    '''
+    @wraps(f) 
+    def decorated(*args, **kwargs): 
+        token = None
+        # jwt is passed in the request header 
+        if 'x-access-token' in request.headers: 
+            token = request.headers['x-access-token'] 
+        # return 401 if token is not passed 
+        if not token: 
+            return jsonify({'message' : 'Token is missing.'}), 401
+   
+        print("********** DECORATOR - TOKEN **********")
+        print(token)
+        print("********** DECORATOR - DECODED TOKEN **********")
+        print(f"SERET KEY: {APP.secret_key}")
+        print(jwt.decode(token, APP.secret_key, algorithms="HS256")) 
+
+        try: 
+            # decoding the payload to fetch the stored details 
+            data = jwt.decode(token, APP.secret_key, algorithms="HS256") 
+            
+            # TODO - Obtain id of user from database
+            print("********** DATA **********")
+            print(data)
+            current_user = Fighter.find_user_by_attribute("_id", ObjectId(data['id']))
+            print("********** CURRENT USER **********")
+            print(current_user)
+            #find_user_by_attribute(cls, attribute, user_attribute)
+            #current_user = User.query.filter_by(public_id = data['public_id']).first() 
+        except: 
+            return make_response(
+                dumps(
+                    {"message": "Token is invalid."}
+                ), 
+                401
+            ) 
+        # returns the current logged in users contex to the routes 
+        return  f(current_user, *args, **kwargs) 
+   
+    return decorated 
+
+
+@APP.route('/auth/register', methods=['POST'])
+@cross_origin()
+def register_user():
+    '''
+    Registers a user
+    '''
+    data = request.get_json()
+    result = auth_register(data, APP.secret_key)
+    return result
+
+
+@APP.route('/auth/login', methods=['POST'])
+@cross_origin()
+def login_user():
+    '''
+    Logs in a user
+    '''
+    data = request.get_json()
+    result = auth_login(data, APP.secret_key)
+    return result
+
+
+'''
+To logout, tokens should be removed from the client side cookie
+https://stackoverflow.com/questions/21978658/invalidating-json-web-tokens
+@APP.route('/auth/logout', methods=['POST'])
+def logout_user():
+    data = request.get_json()
+    result = auth_logout(APP.secret_key)
+    return result
+'''
+
+
 
 ########## FIGHTERS ROUTES ##########
 @APP.route('/fighters', methods=['GET'])
+@cross_origin()
 def get_fighters():
     '''
     Returns list of figher objects
